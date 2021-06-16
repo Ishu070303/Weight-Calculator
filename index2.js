e0bfaf36655477e8463396cbf18764d19535363c
eshika rawat <83294825+ihshuashu02@users.noreply.github.com>
2021-06-16 14:41:54 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,12 @@
+/*document.getElementById('output').style.visibility='hidden';
+document.getElementById('lbsInput').addEventListener('input',(e)=>{
+    document.getElementById('output').style.visibility='';
+    const lbs= e.target.value;
+    document.getElementById('gramsOutput').innerHTML=lbs/0.0022046;
+    document.getElementById('kgOutput').innerHTML=lbs/2.2046;
+    document.getElementById('ozOutput').innerHTML=lbs*16;
+
+
+});
+
+*/
